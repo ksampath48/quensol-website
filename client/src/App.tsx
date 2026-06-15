3059cdf114d4d9ac6a3d53a04e95df98bcbbd299
kasturi sampath kumar <ksampath48@gmail.com>
2026-06-16 00:14:56 +0530

    feat: add Blog, Locations, Samples, FAQ, Testimonials, Compare, WhatsApp widget

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { StickyQuoteForm } from "@/components/layout/StickyQuoteForm";
+import { WhatsAppWidget } from "@/components/layout/WhatsAppWidget";
 import Home from "@/pages/Home";
 import Catalog from "@/pages/Catalog";
 import ProductDetails from "@/pages/ProductDetails";
 import Admin from "@/pages/Admin";
 import Legal from "@/pages/Legal";
+import Blog from "@/pages/Blog";
+import BlogPost from "@/pages/BlogPost";
+import Locations from "@/pages/Locations";
+import SampleRequest from "@/pages/SampleRequest";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -18,18 +23,15 @@ function Router() {
       <Route path="/catalog" component={Catalog} />
       <Route path="/product/:id" component={ProductDetails} />
       <Route path="/admin" component={Admin} />
-      <Route path="/privacy">
-        {() => <Legal page="privacy" />}
-      </Route>
-      <Route path="/terms">
-        {() => <Legal page="terms" />}
-      </Route>
-      <Route path="/shipping">
-        {() => <Legal page="shipping" />}
-      </Route>
-      <Route path="/returns">
-        {() => <Legal page="returns" />}
-      </Route>
+      <Route path="/blog" component={Blog} />
+      <Route path="/blog/:slug" component={BlogPost} />
+      <Route path="/locations" component={Locations} />
+      <Route path="/locations/:city" component={Locations} />
+      <Route path="/samples" component={SampleRequest} />
+      <Route path="/privacy">{() => <Legal page="privacy" />}</Route>
+      <Route path="/terms">{() => <Legal page="terms" />}</Route>
+      <Route path="/shipping">{() => <Legal page="shipping" />}</Route>
+      <Route path="/returns">{() => <Legal page="returns" />}</Route>
       <Route component={NotFound} />
     </Switch>
   );
@@ -42,6 +44,7 @@ function App() {
         <Toaster />
         <Router />
         <StickyQuoteForm />
+        <WhatsAppWidget />
       </TooltipProvider>
     </QueryClientProvider>
   );
